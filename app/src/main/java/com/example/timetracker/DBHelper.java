5c7f89cdc8e80fdb5d0917ef28ce99fcb3b0edd8
varrivoda <varrivoda@yandex.ru>
2022-04-30 04:43:03 +1000

    Added following DB structure:
    TABLE routines (routine_id, routine_name)
    
    TABLE actions (_id, timestamp, date, routine id FOREIGN KEY, quantity)

--- app/src/main/java/com/example/timetracker/DBHelper.java
+++ app/src/main/java/com/example/timetracker/DBHelper.java
@@ -8,25 +8,52 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
+    public static final String TABLE_ROUTINES = "routines";
+    public static final String COLUMN_ROUTINE_ID = "routine_id";
+    public static final String COLUMN_ROUTINE_NAME = "routine_name";
+
+    public static final String TABLE_ACTIONS = "actions";
+    public static final String COLUMN_ACTION_ID = "_id";
+    public static final String COLUMN_ACTION_DATE = "date";
+    public static final String COLUMN_ACTION_ROUTINE_ID = "action_id";
+    public static final String COLUMN_ACTION_QUANTITY = "quantity";
+    public static final String COLUMN_ACTION_TIMESTAMP = "quantity";
+
+
     public DBHelper(Context context) {
         super(context, "test", null, 1);
     }
 
-    public void createNewRoutine(){
+    private void createNewRoutinesTable(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_ROUTINES +" ("
+                + COLUMN_ROUTINE_ID + "INTEGER PRIMARY KEY, "
+                + COLUMN_ROUTINE_NAME + "TEXT" +")"
+        );
+    }
 
+    private void createNewActionsTable(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_ACTIONS +" ("
+                + COLUMN_ACTION_ID + " INTEGER PRIMARY KEY, "
+                + COLUMN_ACTION_TIMESTAMP + " INTEGER , "
+                + COLUMN_ACTION_DATE + " TEXT, "
+                + COLUMN_ACTION_ROUTINE_ID + " INTEGER , "
+                + COLUMN_ACTION_QUANTITY + " INTEGER, "
+                + "FOREIGN KEY (" + COLUMN_ROUTINE_ID
+                + ") REFERENCES " + TABLE_ROUTINES + "(" + COLUMN_ROUTINE_ID+")"
+                +")"
+        );
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + "test_table" + "("
-                + "_id" + " INTEGER PRIMARY KEY, "
-                + "_action" + " TEXT ,"
-                + "_quantity" + " INTEGER)");
+        createNewRoutinesTable(db);
+        createNewRoutinesTable(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + "test_table");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ROUTINES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ACTIONS);
         onCreate(db);
     }
 }
